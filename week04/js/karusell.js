e30ad703d707b2afaad48bc765c145bc6896ebb3
DaneloSteinevski <steinborn.d@googlemail.com>
2014-04-25 20:45:49 +0200

    naja

---
+++ week04/js/karusell.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+	$('#links').click(function(){
+		$('#bilder_reihe li:first:visible').hide(); /* geht leider nicht */
+	});
+
+});
+
+/* hab drüber nachgedacht die bilder jeweils eine id zuzuordnen und darüber mit hide und show funktionen zu arbeiten
+hört sich aber eher umständlich an */
